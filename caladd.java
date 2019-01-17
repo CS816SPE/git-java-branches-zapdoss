81e9dadd0533190b5b2d7c4e5879fa32c15076f6
Kashish Jain <jainkashish96@gmail.com>
2019-01-17 10:15:28 +0530

    added subtraction

--- caladd.java
+++ caladd.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 public class caladd {
 	public static void main(String args[]) {
@@ -17,12 +18,18 @@ public class caladd {
 					  res = a + b;
 					  System.out.print("Result = " + res);
 					  break;
-				case '2': System.exit(0);
+				case '2': System.out.print("Enter two numbers: ");
+					  a = scan.nextFloat();
+					  b = scan.nextFloat();
+					  res = a - b;
+					  System.out.print("Result = " + res);
+					  break;
+				case '3': System.exit(0);
 					  break;
 				default: System.out.print("Invalid choice.");
 					 break;
 			}
 			System.out.print("\n--------------------------------\n");
-		} while (choice != 2);
+		} while (choice != 3);
 	}
 }
