663d92f04d82ebb6aa0ba0b010ce0665242cfd49
Kashish Jain <jainkashish96@gmail.com>
2019-01-17 10:44:25 +0530

    added multiplication

--- caladd.java
+++ caladd.java
@@ -7,8 +7,8 @@ public class caladd {
 
 		do {
 			System.out.print("1. Add two numbers: \n");
-			System.out.print("2. Subtract two numbers: \n");
-			System.out.print("3. Exit\n\n");
+			System.out.print("3. Multiply two numbers: \n");
+			System.out.print("4. Exit\n\n");
 			choice = scan.next().charAt(0);
 			switch(choice) {
 				case '1': System.out.print("Enter two numbers: ");
@@ -17,12 +17,19 @@ public class caladd {
 					  res = a + b;
 					  System.out.print("Result = " + res);
 					  break;
-				case '2': System.exit(0);
+				case '3': System.out.print("Enter two numbers: ");
+					  a = scan.nextFloat();
+					  b = scan.nextFloat();
+					  res = a * b;
+					  System.out.print("Result = " + res);
+					  break;
+				
+				case '4': System.exit(0);
 					  break;
 				default: System.out.print("Invalid choice.");
 					 break;
 			}
 			System.out.print("\n--------------------------------\n");
-		} while (choice != 2);
+		} while (choice != 4);
 	}
 }
